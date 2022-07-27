619f1d949efa89e343a820253a54410c29eb08ef
oksanaPC2 <77opod@gmail.com>
2022-07-27 19:30:33 +0300

    Добавьте файлы проекта.

---
+++ DZ_11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Задача\nРазработать структуру для решения линейного уравнения 0=kx+b. \nКоэффициенты уравнения k, b реализовать с помощью полей вещественного типа. \nДля решения уравнения предусмотреть метод Root. \nСоздать экземпляр разработанной структуры. \nОсуществить использование экземпляра в программе.\n\nВведите количество попыток для этой программы:");
+            int t = 0;
+            try
+            {
+                t = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Ошибка! ");
+                Console.WriteLine(ex.Message);
+            }
+            int o = 1;
+            while (o <= t)
+            {
+                Console.WriteLine("Введите k");
+                double k = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите b");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                make_yravnenie(k, b).check();
+                make_yravnenie(k, b).Root();
+                Console.WriteLine("Нажмите любую кнопку для продолжения!");
+                Console.ReadKey();
+                o++;
+            }
+        }
+        static yravnenie make_yravnenie(double k, double b)
+        {
+            yravnenie yr1 = new yravnenie(k, b);
+            return yr1;
+        }
+    }
+}
---
+++ DZ_11/Root.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_11
+{
+    class yravnenie
+
+    {
+        public double k;
+        public double b;
+
+        public yravnenie(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public void check()
+        {
+            if ((this.k is double) && (this.b is double))
+            {
+                Console.WriteLine("проверка пройдена");
+            }
+            else
+            {
+                Console.WriteLine("проверка не пройдена");
+            }
+
+        }
+        public void Root()
+        {
+            double x = 0;
+            if (this.k != 0)
+            {
+                x = (-this.b / this.k);
+                Console.WriteLine("x={0}", x);
+            }
+            else
+            {
+                if (this.b == 0)
+                {
+                    Console.WriteLine("x - любое число");
+                }
+                else
+                {
+                    Console.WriteLine("уравнение не имеет решения");
+                }
+            }
+        }
+    }
+}
